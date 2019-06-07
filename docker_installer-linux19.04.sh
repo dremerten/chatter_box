edae9f08724da76eb03cbfcb5395001f2e5583b1
dremerten <dremerten@users.noreply.github.com>
2019-06-07 10:25:39 -0700

    Update docker_installer-linux19.04.sh

--- docker_installer-linux19.04.sh
+++ docker_installer-linux19.04.sh
@@ -27,8 +27,8 @@ linux_install(){
     add_gpg_key
     update_source_list
     install_docker_ce
-    printf "\n\n\e[5;44;1;33m*** Attention $(whoami) Please press 'q' to complete installation*** \e[0m\n\e[5;44;1;33\e[0m\n\n"
     add_usr_perms
+    printf "\n\n\e[5;44;1;33m*** Attention $(whoami) Please press 'q' to complete installation*** \e[0m\n\e[5;44;1;33\e[0m\n\n"
     verify_running
 }
 
