278341c80e19997542fa1eabd3bab5aa678543c2
dremerten <dremerten@users.noreply.github.com>
2019-06-07 10:21:31 -0700

    Update docker_installer-linux19.04.sh

--- docker_installer-linux19.04.sh
+++ docker_installer-linux19.04.sh
@@ -16,12 +16,18 @@ verify_running() {
    sudo systemctl status docker
 }
 
+add_usr_perms(){
+   sudo usermod -aG docker ${USER}
+   su ${USER}
+   echo "$(whoami) Please verify you are part of the docker group"
+   id -nG
+}
+
 linux_install(){
     add_gpg_key
     update_source_list
     install_docker_ce
     printf "\n\n\e[5;44;1;33m*** Attention $(whoami) Please press 'q' to complete installation*** \e[0m\n\e[5;44;1;33\e[0m\n\n"
-    sudo usermod -aG docker $(whoami)
     verify_running
 }
 
