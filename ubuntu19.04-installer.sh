d82324323d1ea660bd017321331226ae972d8861
Andre Merten <andre.mertn@ibm.com>
2019-06-13 12:06:17 -0700

    linux 19.04 installer-all-auto added

--- ubuntu19.04-installer.sh
+++ ubuntu19.04-installer.sh
@@ -16,12 +16,29 @@ verify_running() {
    sudo systemctl status docker
 }
 
+
+install_pkgs(){
+  sudo apt-get -y install python-dev gcc libc-dev make
+}
+
+install_dccomps(){
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+  sudo chmod +x /usr/local/bin/docker-compose &&
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose &&
+  docker-compose --version
+}
+
+runner(){
+  install_pkgs
+  install_dccomps
+}
+
 linux_install(){
     add_gpg_key
     update_source_list
     install_docker_ce
-    printf "\n\n\e[5;44;1;33m*** Attention $(whoami) Please press 'q' to complete installation*** \e[0m\n\e[5;44;1;33\e[0m\n\n"
     verify_running
+    runner
 }
 
 linux_install
@@ -31,6 +48,3 @@ printf "\n\n\e[5;44;1;33m*** Attention $(whoami) Please Run the following to add
 echo "1) sudo usermod -aG docker ${USER}"
 echo "2) su ${USER}"
 echo "3) id -nG"
-
-
-
